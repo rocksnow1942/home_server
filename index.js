041a854369d90bf63841e15dbcc6065417ed2f5e
Hui Kang <rocksnow1942@gmail.com>
2020-08-31 02:12:36 -0700

    ON: Mon Aug 31 02:12:35 2020, FROM: mbp

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3337
 
-app.use((req, res, next)=>{
-    console.log(req.url);
-    res.send(req.url)
-    next();
-})
 
 app.get('/',(req,res)=>{
     res.json({msg:'hello world!'})
